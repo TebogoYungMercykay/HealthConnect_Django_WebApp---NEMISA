62f77755ad599d79b8a67f3128d8aef9657c575f
Dan Phala <mrphalad@gmail.com>
2023-11-22 21:34:27 +0200

    root location

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -14,7 +14,7 @@ import os
 from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
