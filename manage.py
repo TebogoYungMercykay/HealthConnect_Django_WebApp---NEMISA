202431a61057624171af934b073364167f0220cb
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-01-13 15:32:58 +0200

    Incomplete: HealthConnect FastAPI Integration

--- manage.py
+++ manage.py
@@ -2,10 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disease_prediction.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthconnect_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
