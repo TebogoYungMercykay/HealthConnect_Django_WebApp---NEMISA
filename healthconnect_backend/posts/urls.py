e3773fab70d022247c49610ea7bc3265488492c6
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-01-27 16:51:31 +0200

    Complete Post Functionality for Patients and Doctors

--- healthconnect_backend/posts/urls.py
+++ healthconnect_backend/posts/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_posts, name='get_posts'),
     path('all_posts' , views.all_posts, name='all_posts'),
     path('create_post' , views.create_post, name='create_post'),
     path('get/<int:post_id>' , views.get_post, name='get_post'),
     path('update/<int:post_id>' , views.update_post, name='update_post'),
     path('delete/<int:post_id>' , views.delete_post, name='delete_post'),
-    path('create_reply/<int:post_id>' , views.create_reply, name='create_reply'),
+    path('create_reply' , views.create_reply, name='create_reply'),
     path('vote' , views.vote, name='vote'),
 ]
