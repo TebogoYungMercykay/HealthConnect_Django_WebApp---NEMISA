285ad73dd64ab30648ea1434232ef431e6b8e5f6
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-01-29 02:05:56 +0200

    Disease prediction & Making Consultations

--- healthconnect_backend/accounts/urls.py
+++ healthconnect_backend/accounts/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('signup_patient', views.signup_patient, name="signup_patient"),
     path('signup_doctor', views.signup_doctor , name="signup_doctor"),
     path('savedata/<int:user_id>', views.savedata , name='savedata'),
-    path('doctors', views.get_doctors , name="get_doctors"),
     path('get_user', views.get_user , name='get_user'),
 ]
