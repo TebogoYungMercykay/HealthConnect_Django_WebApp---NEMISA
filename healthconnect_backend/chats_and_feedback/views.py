72d0cf5b40771457ad47299cac60c1a80e1ad9b6
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-01-29 02:06:08 +0200

    Disease prediction & Making Consultations

--- healthconnect_backend/chats_and_feedback/views.py
+++ healthconnect_backend/chats_and_feedback/views.py
@@ -4,9 +4,7 @@ from django.urls import reverse
 from django.core.exceptions import PermissionDenied
 from django.contrib import messages
 from twilio.rest import Client
-import requests
-import logging
-import os
+import requests, logging, json, os
 
 MESSAGE = "Some Error Occured, Please Try Again."
 USER_MESSAGE = "Incorrect User Id Used, Please Try Again."
@@ -111,99 +109,75 @@ def user_feedback(request, user_id):
         return JsonResponse({'status': 'error', 'message': MESSAGE})
 
 
-def chat_messages(request, user_id):
+def chat_messages(request, user_id, messages):
+    try:
+        param = user_id
+        user_id = request.session.get('user_id')
 
-    if request.method == 'GET' or request.method == 'POST':
-        
-        if not request.POST['messages']:
-            messages.error(request, MESSAGE)
-            return None
-        
-        try:
-            param = user_id
-            user_id = request.session.get('user_id')
-            
-            if user_id != param and user_id != None:
-                
-                jwt_token = request.session.get('access_token')
-                token_type = request.session.get('token_type')
+        if user_id != param and user_id is not None:
+            jwt_token = request.session.get('access_token')
+            token_type = request.session.get('token_type')
 
-                api_url = os.getenv("API_ENDPOINT") + f'/chats/chat_messages/{user_id}'
+            api_url = os.getenv("API_ENDPOINT") + f'/chats/chat_messages/{user_id}'
 
-                headers = {
-                    "Content-Type": JSON_DATA,
-                    "Authorization": f"{token_type} {jwt_token}",
-                }
+            headers = {
+                "Content-Type": JSON_DATA,
+                "Authorization": f"{token_type} {jwt_token}",
+            }
 
-                response = requests.post(api_url, headers=headers)
-                response.raise_for_status()
-                
-                if response.status_code == 200:
-                    api_response = response.json()
-                    if api_response.get('status') == "success":
+            response = requests.post(api_url, headers=headers)
 
-                        return api_response.get('data')
-                
-                logging.error(f"Error Occured When Requesting Chat Data: {e}: User Id: {user_id}")
-                return None
-            
-            else:
-                raise PermissionDenied(USER_MESSAGE)
-      
-        except requests.RequestException as e:
-            logging.error(f"Error Occured When Requesting Chat Data: {e}: User Id: {user_id}")
+            if response.status_code == 200:
+                api_response = response.json()
+                if api_response.get('status') == "success":
+                    return api_response.get('data')
+
+            logging.error(f"Error Occurred When Requesting Chat Data: {response.text}, User Id: {user_id}")
             return None
 
-    else:
-        messages.error(request, METHOD_ERROR)
+        else:
+            messages.error(request, USER_MESSAGE)
+            return None
+
+    except requests.RequestException as e:
+        logging.error(f"Error Occurred When Requesting Chat Data: {e}, User Id: {user_id}")
         return None
 
 
-def create_chat(request, user_id):
-    
-    if request.method == 'POST':
-        
-        try:
-            param = user_id
-            user_id = request.session.get('user_id')
-            
-            if user_id != param and user_id != None:
-                
-                jwt_token = request.session.get('access_token')
-                token_type = request.session.get('token_type')
+def create_chat(request, consultation_id, message, sender_id = None):
+    try:
+        user_id = request.session.get('user_id')
 
-                api_url = os.getenv("API_ENDPOINT") + f'/chats/create_chat/{user_id}'
+        if user_id is not None:
+            jwt_token = request.session.get('access_token')
+            token_type = request.session.get('token_type')
 
-                post_data = {
-                    "message": request.POST['message'],   
-                }
-                
-                headers = {
-                    "Content-Type": JSON_DATA,
-                    "Authorization": f"{token_type} {jwt_token}",
-                }
+            api_url = os.getenv("API_ENDPOINT") + f'/chats/create_chat/{consultation_id}?sender_id={sender_id}'
 
-                response = requests.post(api_url, data=post_data, headers=headers)
-                response.raise_for_status()
-                
-                if response.status_code == 200:
-                    api_response = response.json()
-                    if api_response.get('status') == "success":
+            post_data = {  "message": message, }
+            post_data = json.dumps(post_data, indent=4, ensure_ascii=False)
 
-                        return api_response.get('data')
-                
-                logging.error(f"Error Occured When Requesting Chat Data: {e}: User Id: {user_id}")
-                return None
-            
-            else:
-                raise PermissionDenied(USER_MESSAGE)
-      
-        except requests.RequestException as e:
-            logging.error(f"Error Occured When Requesting Chat Data: {e}: User Id: {user_id}")
+            headers = {
+                "Content-Type": JSON_DATA,
+                "Authorization": f"{token_type} {jwt_token}",
+            }
+
+            response = requests.post(api_url, data=post_data, headers=headers)
+
+            if response.status_code == 200:
+                api_response = response.json()
+                if api_response.get('status') == "success":
+                    return api_response.get('data')
+
+            logging.error(f"Error Occurred When Requesting Chat Data: {response.text}, User Id: {user_id}")
             return None
 
-    else:
-        messages.error(request, METHOD_ERROR)
+        else:
+            messages.error(request, USER_MESSAGE)
+            return None
+
+    except requests.RequestException as e:
+        logging.error(f"Error Occurred When Requesting Chat Data: {e}, User Id: {user_id}")
         return None
 
 def whatsapp(request):
