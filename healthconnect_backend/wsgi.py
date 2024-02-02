cd57dbea4cfbff8e3ed54e444850f15882741cf5
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-02-03 01:35:08 +0200

    Added the Logic for Sending Emails from the Contact form

--- healthconnect_backend/wsgi.py
+++ healthconnect_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for disease_prediction project.
+WSGI config for healthconnect_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disease_prediction.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthconnect_backend.settings')
 
 application = get_wsgi_application()
