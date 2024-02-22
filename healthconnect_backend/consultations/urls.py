315e793ca2faf68d81b81a6ccf81757a86a47a7c
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-02-22 05:39:51 +0200

    Integrating the Prediction Rating Feature

--- healthconnect_backend/consultations/urls.py
+++ healthconnect_backend/consultations/urls.py
@@ -8,8 +8,18 @@ urlpatterns = [
     path('consultation_view/<int:consultation_id>', views.consultation_view , name='consultation_view'),
     path('close_consultation/<int:consultation_id>', views.close_consultation , name='close_consultation'),
     path('create_review/<int:doctor_id>', views.create_review , name='create_review'),
+    path('rate_prediction/<int:consultation_id>', views.rate_prediction , name='rate_prediction'),
     path('get_reviews_id/<int:doctor_id>', views.get_reviews_id , name='get_reviews_id'),
     path('get_reviews', views.get_reviews , name='get_reviews'),
     path('videoconsultation/<int:consultation_id>',views.lauchvideocaller,name='consultation_call'),
     path('videoconsultation_live/<int:consultation_id>',views.getToken,name='open_video_consultation')
 ]  
+
+# {
+#     "doctor_id": 10,
+#     "diseaseinfo_id": 43,
+#     "consultation_id": 17,
+#     "rating": 4,
+#     "symptoms": ["Fever", "Headache", "Fatigue"],
+#     "diseasename": "Common Cold"
+# }
